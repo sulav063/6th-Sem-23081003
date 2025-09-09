ed3e23257894eff47ec195c8376a5192b875cd4b
Sulav Adhikari <sulav057@gmail.com>
2025-09-09 09:57:10 +0545

    lab

---
+++ Software Engineering/Lab 2/men-app/src/models/Task.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface ITask extends Document {
+    _id: Types.ObjectId;
+    title: string;
+    description?: string;
+    completed: boolean;
+    priority: "low" | "medium" | "high";
+    createdBy: mongoose.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const TaskSchema = new Schema<ITask>(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 100,
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        completed: {
+            type: Boolean,
+            default: false,
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "medium",
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Task = mongoose.model<ITask>("Task", TaskSchema);
